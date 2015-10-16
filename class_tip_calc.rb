40f73f2ef5f9f7ed017d5137cf0743e423d733c9
Dominique Miller <mr1monkey@Dominiques-MacBook-Air.local>
2015-10-16 13:26:05 -0400

    first tip calc

--- class_tip_calc.rb
+++ class_tip_calc.rb
@@ -1,40 +1,42 @@
 class Calculate
-  attr_accessor :bill, :tip, :tip_percent, :total_bill, :people
-  def initialize(bill=0, tip=0, tip_percent=0, total_bill=0)
+  attr_accessor :bill, :percent, :people
+  def initialize
      @bill = bill
-     @tip = tip
-     @tip_percent = tip_percent
-     @total_bill = total_bill
+     @percent = percent
      @people = people
-
   end
 
-  # def total
-  #   tip_calc.total_bill = ( tip_calc.bill *  tip_calc.tip_percent)/ tip_calc.people
-  #  end
+  def tip
+    bill * percent
+  end
+
+  def total_bill
+    bill + tip
+  end
+
+  def split
+    bill / people
+  end
 end
 
-tip_calc = Calculate.new
+calc = Calculate.new
 
 puts "How much is your bill?"
-tip_calc.bill = gets.chomp.to_f
+calc.bill = gets.chomp.to_f
 
 puts "How many people are paying?"
-tip_calc.people = gets.chomp.to_i
+calc.people = gets.chomp.to_i
 
 puts "What percent tip would you like?"
 answer = gets.chomp.to_f
-tip_calc.tip_percent = answer / 100
+calc.percent = answer / 100
 
-tip_amount = ( tip_calc.bill *  tip_calc.tip_percent)
-tip_calc.total_bill = tip_calc.bill + tip_amount
-split = tip_calc.total_bill / tip_calc.people
 
 puts "Thank you for dining at Wyncafe!"
 puts "********************************"
-puts "You bill is      $#{ tip_calc.bill}"
-puts "The tip is       $#{ tip_amount}"
-puts "The final total: $#{ tip_calc.total_bill}"
-puts "Split total each $#{split}"
+puts "You bill is      $#{calc.bill}"
+puts "The tip is       $#{calc.tip}"
+puts "The final total: $#{calc.total_bill}"
+puts "Split total each $#{calc.split}"
 puts "********************************"
 puts "Thank you, have a nice day!"
